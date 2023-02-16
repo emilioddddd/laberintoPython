4350c3dd1edda9ffb8ce4a8cb87e1a2304a47041
EMILIO GALINDO FELIPE <Emilio.Galindo@alu.uclm.es>
2023-02-16 13:14:15 +0100

    Enviar todas las clases

---
+++ Model/Habitación.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from ElementoMapa import ElementoMapa
+
+class Habitación(ElementoMapa):
+    """Este es concreateComplement"""
+    def __init__(self, num):
+        self.norte = None
+        self.sur = None
+        self.este = None
+        self.oeste = None
+        self.num = num
+
---
+++ Model/Laberinto.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Laberinto:
+    def __init__(self):
+        self.habitaciones = List()
+    def agragarHabitacion(self, hab):
+        self.habitaciones.append(hab)
